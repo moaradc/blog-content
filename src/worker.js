c30b8ab9f50f7b3081a0cd4f15c66916080c1b87
moaradc <143486936+moaradc@users.noreply.github.com>
2026-08-03 05:26:38 +0000

    refactor: 删除 Worker 的 content 正文预览

--- src/worker.js
+++ src/worker.js
@@ -122,13 +122,6 @@ function renderCard(post, index, origin) {
   html += '<a class="item__cta secondary" href="' + escapeAttr(mdUrl) + '">查看 Markdown</a>';
   html += '</div>';
 
-  if (post.content && post.content.trim()) {
-    html += '<details class="item__preview">';
-    html += '<summary>展开正文预览</summary>';
-    html += '<div class="preview-body" style="white-space: pre-wrap;">' + escapeHtml(post.content) + '</div>';
-    html += '</details>';
-  }
-
   html += '</div></article>';
 
   return html;
